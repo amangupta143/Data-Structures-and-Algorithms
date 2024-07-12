8e051155c3bd1044e34431705fabd98a803bbe2c
Aman Gupta <109453339+amangupta143@users.noreply.github.com>
2024-07-12 18:18:50 +0530

    Added solution of 1818
    Added solution of Leetcode problem 1818-maximum-score-from-removing-substrings

---
+++ Leetcode-Solutions/1818-maximum-score-from-removing-substrings/maximum-score-from-removing-substrings.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int maximumGain(string s, int x, int y) {
+        // Ensure "ab" always has more points than "ba"
+        if (x < y) {
+            int temp = x;
+            x = y;
+            y = temp;
+            reverse(s.begin(), s.end());
+        }
+
+        int aCount = 0, bCount = 0, totalPoints = 0;
+
+        for (int i = 0; i < s.size(); ++i) {
+            char currentChar = s[i];
+
+            if (currentChar == 'a') {
+                ++aCount;
+            } else if (currentChar == 'b') {
+                if (aCount > 0) {
+                    --aCount;
+                    totalPoints += x;
+                } else {
+                    ++bCount;
+                }
+            } else {
+                totalPoints += min(bCount, aCount) * y;
+                aCount = bCount = 0;
+            }
+        }
+
+        totalPoints += min(bCount, aCount) * y;
+        return totalPoints;
+    }
+};
