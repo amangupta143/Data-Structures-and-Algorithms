0767dd2c2d0978ee7e17fd35916496d4fa55b108
Aman Gupta <109453339+amangupta143@users.noreply.github.com>
2025-06-10 21:31:38 +0530

    Update contains-duplicate.cpp

--- Leetcode-Solutions/0217-contains-duplicate/contains-duplicate.cpp
+++ Leetcode-Solutions/0217-contains-duplicate/contains-duplicate.cpp
@@ -1,9 +1,12 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        for(int i=0; i<nums.size()-1; i++) {
-            if(nums[i]==nums[i+1]) return true;
+        int size = nums.size();
+        unordered_map<int, int> mp;
+
+        for(int i=0; i<size; i++) {
+            if(mp.find(nums[i]) != mp.end()) return true;
+            mp.insert({nums[i], 1});
         }
         return false;
     }
