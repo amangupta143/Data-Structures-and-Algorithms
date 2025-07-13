485839a3c1a3b576b70e5a300805c8f4ebd12800
amangupta143 <109453339+amangupta143@users.noreply.github.com>
2025-07-13 12:30:09 +0530

    Added Solution for Sort Colors

--- Leetcode-Solutions/0075-sort-colors/sort-colors.cpp
+++ Leetcode-Solutions/0075-sort-colors/sort-colors.cpp
@@ -2,18 +2,20 @@ class Solution {
 public:
     void sortColors(vector<int>& nums) {
         int size = nums.size();
-        int red = 0;
-        int white = 0;
-        
-        for(int i=0; i<size; i++) {
-            if(nums[i]==0) red++;
-            if(nums[i]==1) white++;
-        }
+        vector<int> colors(3, 0);
 
         for(int i=0; i<size; i++) {
-            if(red-->0) nums[i]=0;
-            else if(white-->0) nums[i]=1;
-            else nums[i]=2;
+            colors[nums[i]]++;
+        }
+        
+        int j = 0, i=0;
+        while(j < 3) {
+            if(colors[j] > 0) {
+                nums[i++] = j;
+                colors[j]--;
+            } else {
+                j++;
+            }
         }
     }
 };
