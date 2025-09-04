303d2b8cb16615f8542bf9d14200980951c079dc
Aman Gupta <109453339+amangupta143@users.noreply.github.com>
2025-09-04 23:46:59 +0530

    Time: 0 ms (100.00%) | Memory: 16.7 MB (80.79%) - LeetSync

---
+++ 2058-concatenation-of-array/concatenation-of-array.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        int size = nums.size();
+        vector<int> ans(2*size);
+        for(int i=0; i<size; i++) {
+            ans[i] = nums[i];
+            ans[i+size] = nums[i];
+        }
+        return ans;
+    }
+};
